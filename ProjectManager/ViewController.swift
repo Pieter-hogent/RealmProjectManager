505a49dfebbf79d41800bcc6aca49aad39d1b996
Pieter Van Der Helst <pieter@heyheyapps.com>
2018-11-11 18:48:33 +0100

    Initial Commit

---
+++ ProjectManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectManager
+//
+//  Created by Pieter Van Der Helst on 11/11/2018.
+//  Copyright © 2018 Pieter Van Der Helst. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
